cf89534901000fbbca26e31869d5b165178907c4
zombiecraft2 <51192264+zombiecraft2@users.noreply.github.com>
2022-03-13 10:37:05 +0100

    Update index.js

--- index.js
+++ index.js
@@ -22,24 +22,7 @@ client.on("ready", () => {
 
 client.on("messageCreate", (message) => {
   if (message.author.bot) return;
-
-  if (IsAddingADescription == true) {
-    if (message.author.id == IsAddingDescriptionClient) {
-      if (message.channel == IsAddingDescriptionChannel) {
-        IsAddingADescription = false;
-
-        message.channel.send({
-          embeds: [
-            {
-              description: "Your party has been created succesfully !",
-              color: "LUMINOUS_VIVID_PINK",
-            },
-          ],
-        });
-      }
-    }
-  }
-
+  
   var args = message.content.split(" ");
 
   if (args[0]) {
@@ -51,38 +34,7 @@ client.on("messageCreate", (message) => {
             color: "GREEN",
           },
         ],
-      });
-
-      message.channel.send({
-        embeds: [
-          {
-            description: "Add a description so it more specific !",
-            color: "LUMINOUS_VIVID_PINK",
-          },
-        ],
-      });
-      var lastBotMsg = message.channel.lastMessage;
-
-      IsAddingDescriptionChannel = message.channel;
-      IsAddingDescriptionClient = message.author.id;
-      IsAddingADescription = true;
-
-      function DestroyMessageIfNoDescription() {
-        if (IsAddingADescription) {
-          IsAddingADescription = false;
-
-          lastBotMsg.reply({
-            embeds: [
-              {
-                description: "Message expired !",
-                color: "DARK_RED",
-              },
-            ],
-          });
-        }
-      }
-
-      setTimeout(DestroyMessageIfNoDescription, 20000);
+    });
     }
   }
 });
